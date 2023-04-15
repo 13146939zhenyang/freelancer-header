c1b3fc71dd1add59c630678675bf6956cb5e4c7f
Zhen Yang <webyangfullstack@gmail.com>
2023-04-15 17:16:19 +1000

    modify css

--- components/Home/HomeContent.tsx
+++ components/Home/HomeContent.tsx
@@ -6,10 +6,10 @@ import { SectionWrapper } from '../Hoc'
 const HomeContent = () => {
 	return (
 		<>
-			<div className='mb-20 lg:text-[95px] text-2xl'>
-				<motion.section variants={fadeIn('spring', 'top', 1 * 0.5, 0.75)}>Pioneers in zzzz</motion.section>
-				<motion.section variants={fadeIn('spring', 'top', 2 * 0.5, 0.75)}>service &</motion.section>
-				<motion.section variants={fadeIn('spring', 'top', 3 * 0.5, 0.75)}>zzz services</motion.section>
+			<div className='mb-20 lg:text-[95px] text-2xl lg:mb-40'>
+				<motion.section variants={fadeIn('spring', 'top', 1 * 0.5, 0.75)} className='mb-2 lg:mb-20'>Pioneers in zzzz</motion.section>
+				<motion.section variants={fadeIn('spring', 'top', 2 * 0.5, 0.75)} className='mb-2 lg:mb-20'>service &</motion.section>
+				<motion.section variants={fadeIn('spring', 'top', 3 * 0.5, 0.75)} className='mb-2 lg:mb-20'>zzz services</motion.section>
 			</div>
 			<motion.section variants={fadeIn('spring', 'bottom', 4 * 0.5, 0.75)}>
 				<button className='border-white border-2 lg:px-10 px-4 lg:py-4 py-2 font-semibold rounded-2xl lg:text-xl text-sm'>Request a zzzz service machine</button>
